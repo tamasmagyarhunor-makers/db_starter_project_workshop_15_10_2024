37050cce1382a102df57ba42b874b7c8703b5c54
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2024-10-15 11:17:36 +0100

    Add extra steps in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
+from lib.artist import Artist
 
 
 # Connect to the database
@@ -9,10 +10,16 @@ connection.connect()
 # Seed with some seed data
 connection.seed("seeds/music_library.sql")
 
+artist = Artist(3, "Celldweller", "rock")
+
 # Retrieve all artists
 artist_repository = ArtistRepository(connection)
+artist_repository.create(artist)
+
 artists = artist_repository.all()
 
 # List them out
 for artist in artists:
     print(artist)
+
+    # we expect Celldweller to show up last.
